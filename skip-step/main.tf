6cec7d8fc8cf40f16f7124419630e9e70f2e32f9
Liaquat Ali <ali.uibgmail.com>
2022-02-20 17:12:06 +0100

    backend

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -1,12 +1,17 @@
-terraform {
-  backend "azurerm" {
-  }
-}
+
 
 provider "azurerm" {
   version = ">=2.0"
   # The "feature" block is required for AzureRM provider 2.x.
   features {}
+}
+terraform{
+backend "azurerm" {
+    resource_group_name  = "resourcegroup-tfstate"
+    storage_account_name = "terraformtfstateskipqa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 resource "azurerm_resource_group" "rg" {
